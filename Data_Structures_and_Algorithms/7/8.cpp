b171b6d6c34d136ce6b9e25f1485a5f530e21aeb
Zhizhong Yan <A2564011261@163.com>
2024-10-06 15:43:12 +0800

    add

--- Data_Structures_and_Algorithms/7/8.cpp
+++ Data_Structures_and_Algorithms/7/8.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<long long, long long> map1[10020][10020];   // dis, cost
+pair<long, long> map1[10003][10003];   // dis, cost
+int re[10003][10003];
 
 struct node {
-    long long curDis;
-    long long curCost;
+    long curDis;
+    long curCost;
 };
+
+bool record[10003], isfind;
 
 node res = {LONG_MAX, LONG_MAX}, cur;
 
 void dfs(int curId, int N, int K) {
-    if(cur.curCost > K) return;
     if(curId == N) {
-        if(cur.curCost <= K && cur.curDis < res.curDis) {
+        if(cur.curDis < res.curDis) {
             res.curCost = cur.curCost;
             res.curDis = cur.curDis;
+            isfind = true;
         }
         return;
     }
     for(int i = 1; i <= N; ++i) {
-        if(map1[curId][i].first != 0) {
+        if(map1[curId][i].first != 0 && record[i] == false) {
+            if(cur.curCost + map1[curId][i].second > K) continue;
+            if(cur.curDis + map1[curId][i].first > res.curDis+20) continue;
             cur.curDis += map1[curId][i].first;
             cur.curCost += map1[curId][i].second;
+            record[curId] = true;
+            re[curId][i] = cur.curDis;
             dfs(i, N, K);
+            record[curId] = false;
             cur.curDis -= map1[curId][i].first;
             cur.curCost -= map1[curId][i].second;
         }
@@ -36,11 +44,13 @@ int main() {
     cout.tie(nullptr);
     int K, N, R, S, D, L, T;    // K: 花费的最大的金币数, N: 城市的数目, R: 路的数目
     cin >> K >> N >> R;
-    for(long long i = 1; i <= R; ++i) {
+    for(long i = 1; i <= R; ++i) {
         cin >> S >> D >> L >> T;
-        map1[S][D] = make_pair(L, T);
+        if(S != D) map1[S][D] = make_pair(L, T);
     }
     cur = {0, 0};
+    record[1] = true;
 	dfs(1, N, K);
-    cout << res.curDis;
+    if(isfind) cout << res.curDis<<endl;
+    else cout << "-1"<<endl;
 }
