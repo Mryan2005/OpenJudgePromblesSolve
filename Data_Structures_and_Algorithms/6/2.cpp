86fc4abd2176a59a44dc12981dc7d4e768696856
Zhizhong Yan <A2564011261@163.com>
2024-10-07 16:35:40 +0800

    add

--- Data_Structures_and_Algorithms/6/2.cpp
+++ Data_Structures_and_Algorithms/6/2.cpp
@@ -2,21 +2,55 @@
 using namespace std;
 
 struct node {
-    char data;
-    node *l, *r;
-};
+    unordered_map<char, node*> mp;
+    int count, isEnd;
+} root;
 
-node *initTree() {
-    return NULL;
+vector<pair<string,string>> res;
+
+void abjust(string s) {
+    node *p = &root;
+    for(int i = 0; i < s.size(); ++i) {
+        if(p->mp.find(s[i]) != p->mp.end()) {
+            ++(p->count);
+            p = p->mp[s[i]];
+        } else {
+            p->mp[s[i]] = new node();
+            ++(p->count);
+            p = p->mp[s[i]];
+        }
+    }
+    p->isEnd = 1;
+    p->count += 1;
 }
 
-
-void createATree(node **T, string s, int i) {
-
-    createATree(&(*T)->l, s, i);
+void findShortestPrefixes(string s) {
+	node *p = &root;
+	int count = 0;
+	for(int i = 0; i < s.size(); ++i) {
+		if(p->count == 1) {
+			res.push_back(make_pair(s, s.substr(0, count)));
+			return;
+		}
+		p = p->mp[s[i]];
+		++count;
+	}
+	res.push_back(make_pair(s, s.substr(0, count)));
 }
 
 int main() {
-    node *T = initTree();
-
+    string s;
+	vector<string> v;
+    while(cin >> s) {
+		v.push_back(s);
+    }
+	for(auto i: v) {
+		abjust(i);
+	}
+	for(auto i: v) {
+		findShortestPrefixes(i);
+	}
+    for(auto i: res) {
+        cout << i.first << " " << i.second << endl;
+    }
 }
