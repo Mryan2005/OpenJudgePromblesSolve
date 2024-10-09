8e423ce35717e72a92d52d88ea2c784510ff43ae
Zhizhong Yan <A2564011261@163.com>
2024-10-09 13:45:35 +0800

    add

--- Data_Structures_and_Algorithms/6/3.cpp
+++ Data_Structures_and_Algorithms/6/3.cpp
@@ -2,84 +2,51 @@
 using namespace std;
 
 struct node {
-    char data[100];
-    node *l, *r;
-};
+    map<string, node*> mp;
+} root;
 
-node *initTree() {
-    return NULL;
-}
-
-vector<string> processInput(string path) {
-    string cur;
-    vector<string> res;
-    int i = 0, j = 0;
-    for(; i < path.size(); ++i) {
-        if(path[i] == '\\') {
-            cur = path.substr(j, i-j);
-            res.insert(res.end(), cur);
-            j = i+1;
+void solve(string s) {
+    string subS;
+    node *p = &root;
+    int j = 0, k = j;
+    for(; j < s.size(); ++j) {
+        if(s[j] == '\\') {
+            subS = s.substr(k, j-k);
+            if(p->mp.find(subS) == p->mp.end()) {
+                p->mp[subS] = new node;
+                p = p->mp[subS];
+            } else {
+                p = p->mp[subS];
+            }
+			k = j+1;
         }
     }
-    cur = path.substr(j, i-j);
-    res.insert(res.end(), cur);
-    return res;
+    subS = s.substr(k, j-k);
+    if(p->mp.find(subS) == p->mp.end()) {
+        p->mp[subS] = new node;
+        p = p->mp[subS];
+    } else {
+        p = p->mp[subS];
+    }
 }
 
-void addNode(node **T, vector<string> res) {
-    node *p = *T;
-    for(int i = 0; i < res.size(); ++i) {
-        if(p == NULL) {
-            if(i == 0) {
-                *T = (node*)malloc(sizeof(node));
-                strcpy((*T)->data, res[i].c_str());
-                (*T)->l = (*T)->r = NULL;
-				p = *T;
-            }
-        } else if(p && p->l == NULL) {
-            p->l = (node*)malloc(sizeof(node));
-            p = p->l;
-            strcpy(p->data, res[i].c_str());
-            p->l = p->r = NULL;
-        } else {
-            while(1) {
-                if(strcmp(res[i].c_str(), p->data) > 0) {
-                    p = p->r;
-                } else if(strcmp(res[i].c_str(), p->data) == 0) {
-                    if(p->l) p = p->l;
-					else break;
-                } else if(strcmp(res[i].c_str(), p->data) < 0) {
-                    node *q = (node*)malloc(sizeof(node));
-                    strcpy(q->data, res[i].c_str());
-                    q->l = q->r = NULL;
-					if(p == *T) {
-						q->r = (*T);
-						*T = q;
-						p = (*T);
-					} else {
-	                    q->r = p;
-	                    p->r = q;
-	                    p = q;
-					}
-                }
-            }
-        }
-    }
+void outputRes(node *curP, int f) {
+	for(auto i: curP->mp) {
+		for(int i = 1; i <= f; ++i) {
+			cout << " ";
+		}
+		cout << i.first << endl;
+		outputRes(i.second, f+1);
+	}
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int n;
-    string path;
-    node *T;
-    cin >> n;
-    T = initTree();
-    for(int i = 1; i <= n; ++i) {
-        cin >> path;
-        vector<string> res = processInput(path);
-        addNode(&T, res);
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; ++i) {
+        string s, subS;
+        cin >> s;
+        solve(s);
     }
-    // printDir();
+	outputRes(&root, 0);
 }
